2eb467a1cbbcf7c5e43f387115a4aeb47dc06744
Sam <z5588327@ad.unsw.edu.au>
2025-08-28 13:03:48 +1000

    Send model to index using thymeleaf

---
+++ src/main/java/com/example/store/controller/MessageController.java
@@ -0,0 +1,12 @@
+package com.example.store.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @RequestMapping("/hello")
+        public String sayHello() {
+            return "Hello World!";
+        }
+}
