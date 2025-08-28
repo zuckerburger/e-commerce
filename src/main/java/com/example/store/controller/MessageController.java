317388e48163d353f7796917dba40ee50a9b138f
Sam <z5588327@ad.unsw.edu.au>
2025-08-28 13:27:34 +1000

    Simple REST API endpoint

--- src/main/java/com/example/store/controller/MessageController.java
+++ src/main/java/com/example/store/controller/MessageController.java
@@ -3,10 +3,12 @@ package com.example.store.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.store.entities.Message;
+
 @RestController
 public class MessageController {
     @RequestMapping("/hello")
-        public String sayHello() {
-            return "Hello World!";
+        public Message sayHello() {
+            return new Message("Hello World!");
         }
 }
---
+++ src/main/java/com/example/store/entities/Message.java
@@ -0,0 +1,10 @@
+package com.example.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String text;
+}
